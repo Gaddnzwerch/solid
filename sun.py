8c94652344a303064acde42de7c7f75bc1417678
Andreas Schultz <a.schultz@koppermann.com>
2015-06-25 18:01:11 +0200

    some cleanup

--- sun.py
+++ sun.py
@@ -1,7 +1,7 @@
 import orbit
 
 class Sun():
-  #defaul is diameter of our sun
+  #default is diameter of our sun
   def __init__(self, a_diameter = 1392684, a_mass = 1.989e30):
     self.__diameter = a_diameter # km
     self.__mass = a_mass # in kg
