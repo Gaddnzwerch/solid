a3bdc97b5a4e1b44d66bbee493564b9eb939fa6d
Andreas Schultz <a.schultz@koppermann.com>
2015-07-10 15:29:41 +0200

    minor changes

--- planet.py
+++ planet.py
@@ -2,33 +2,27 @@ import collections
 
 class Planet:
     def __init__(self, a_diameter = 1275632, a_mass = 5.974e24):
-        self.__diameter = a_diameter
-        self.__mass = a_mass #in kg
-
-    def get_diameter(self):
-        return self.__diameter
-    diameter = property(get_diameter)
-
-    def get_mass(self):
-        return self.__mass
-    mass = property(get_mass)
+        self.diameter = a_diameter
+        self.mass = a_mass #in kg
+        self.surfaceMap = None
 
 class PlanetFactory:
+    #3254473
 
     def create_earth():
-        return Planet(1275632,5.974e24)
+        return Planet(12756.32,5.974e24)
     create_earth = staticmethod(create_earth)
 
     def create_mercury():
-        return Planet(4879400,3.301e23)
+        return Planet(4879.400,3.301e23)
     create_mercury = staticmethod(create_mercury)        
 
     def create_venus():
-        return Planet(12103600,4.869e24)
+        return Planet(12103.600,4.869e24)
     create_venus = staticmethod(create_venus)        
 
     def create_mars():
-        return Planet(6792400, 6.418e23)
+        return Planet(6792.400, 6.418e23)
     create_mars = staticmethod(create_mars)
 
     def create_jupiter():
